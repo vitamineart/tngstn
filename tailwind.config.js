c0e4087ad4be2034dd43530ce0aba0b08c431efb
Art <vitaminearte@gmail.com>
2022-03-02 21:14:30 +0200

    fixed ads grid jumbled on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,64 @@
 module.exports = {
-  content: ['./src/pug/**/*.pug', './src/js/**/*.js', './src/**/*.html'],
+  content: ["./src/pug/**/*.pug", "./src/js/**/*.js", "./src/**/*.html"],
   corePlugins: {
-     container: false,
+    container: false,
   },
   theme: {
     extend: {
       colors: {
-         blue: {
-          light: '#338bc8',
-          link: '#2581d0',
-          DEFAULT: '#185599',
-          dark: '#071f51',
+        blue: {
+          light: "#338bc8",
+          link: "#2581d0",
+          DEFAULT: "#185599",
+          dark: "#071f51",
         },
         indigo: {
-          light: '#76769f',
-          DEFAULT: '#121341',
+          light: "#76769f",
+          DEFAULT: "#121341",
         },
         gray: {
-          light: '#ebebeb',
-          dark: '#bcbcbc'
-        }
+          light: "#ebebeb",
+          dark: "#bcbcbc",
+        },
       },
       borderWidth: {
-        DEFAULT: '1px',
-        '0': '0',
-        '2': '2px',
-        '3': '3px',
-        '4': '4px',
-        '5': '5px',
-        '6': '6px',
-        '8': '8px',
-        '12': '12px',
+        DEFAULT: "1px",
+        0: "0",
+        2: "2px",
+        3: "3px",
+        4: "4px",
+        5: "5px",
+        6: "6px",
+        8: "8px",
+        12: "12px",
       },
       boxShadow: {
-        'cardshadow': '0 1px 45px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.2)',
+        cardshadow:
+          "0 1px 45px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.2)",
       },
       fontFamily: {
-        'proxima': 'ProximaNovaA-Regular, sans-serif',
-        'proxima-bold': 'ProximaNovaA-Bold, sans-serif',
+        proxima: "ProximaNovaA-Regular, sans-serif",
+        "proxima-bold": "ProximaNovaA-Bold, sans-serif",
       },
       willChange: {
-        'transform-opacity': 'transform, opacity',
-        'opacity': 'opacity',
+        "transform-opacity": "transform, opacity",
+        opacity: "opacity",
       },
       transitionProperty: {
-        'transform-opacity': 'transform, opacity',
+        "transform-opacity": "transform, opacity",
+      },
+      spacing: {
+        12.5: "3.125rem",
+        13: "3.25rem",
+        82: "20.5rem",
+        84: "21rem",
+        86: "21.5rem",
+        88: "22rem",
+        90: "22.5rem",
+        92: "23rem",
+        94: "23.5rem",
       },
     },
   },
   plugins: [],
-}
+};
