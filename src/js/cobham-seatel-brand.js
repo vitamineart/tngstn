30f34a2fb0dc377b75bfe6a91371d31e9aea2b5e
Art <vitaminearte@gmail.com>
2022-05-20 18:15:19 +0300

    changed hero video

--- src/js/cobham-seatel-brand.js
+++ src/js/cobham-seatel-brand.js
@@ -11,3 +11,13 @@ ships.forEach(ship => {
         shipText.classList.remove('highlighted')
     })
 })
+
+
+
+const video = document.querySelector('video')
+// Позже, после выполнения некоторых условий, устанавливаем в качестве
+// источника URL-адрес предварительно загруженного видео.
+video.src = 'img/cobham-brand/cobham-brand-hero-bg.mp4';
+video.play().then(() => {
+    // Если URL предварительно загруженного видео уже кэширован, воспроизведение начинается сразу же.
+});
