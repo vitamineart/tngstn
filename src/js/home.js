62f62d39226f528d5ac7b32e1ad9b380fb14e84a
Art <vitaminearte@gmail.com>
2023-02-23 18:37:27 +0200

    tom-meyer:checked image filenames, alts, SEO tags

--- src/js/home.js
+++ src/js/home.js
@@ -1,15 +1,30 @@
 const content = document.querySelector(".page-content");
-const contentItems = content.querySelectorAll('.expander > .container > *');
+const contentItems = content.querySelectorAll('.expander .container > *');
 const readMoreButton = document.getElementById("read-more-button");
 const readMoreButtonText = document.querySelector("#read-more-button span.text");
+
 
 readMoreButton.addEventListener("click", function ({target}) {
     readMoreButton.classList.toggle('opened');
     if(content.clientHeight == 0) {
         content.style.height = `${content.scrollHeight}px`;
-        gsap.from(contentItems, {y:10, scale: 0.9, opacity: 0, duration: .6, stagger: .1, });
+        gsap.from(contentItems, {y:10, scale: 0.9, opacity: 0, duration: 2, stagger: .1, });
 
-        readMoreButton.scrollIntoView({behavior: "smooth"});
+
+        // readMoreButton.scrollIntoView({behavior: "smooth"});
+        gsap.to(window, {
+            duration: 2,
+            ease: 'power2.inOut',
+            scrollTo: {
+                y: readMoreButton,
+                offsetY: 15,
+                autoKill: true,
+            }
+        })
+
+        // var scrollButton = readMoreButton.offsetTop;
+        // window.scrollTo({ top: scrollButton, behavior: 'smooth',  });
+
         readMoreButtonText.textContent = 'See Our Work';
     } else {
         content.style.height = 0
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-
+gsap.registerPlugin(ScrollToPlugin);
 
 
 // Set a variable for our button element.
@@ -20,26 +20,16 @@ const scrollFunc = () => {
 };
 
 window.addEventListener("scroll", scrollFunc);
-
-const scrollToTop = () => {
-  // Let's set a variable for the number of pixels we are from the top of the document.
-  const c = document.documentElement.scrollTop || document.body.scrollTop;
-
-  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
-  // We'll also animate that scroll with requestAnimationFrame:
-  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
-  if (c > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    // ScrollTo takes an x and a y coordinate.
-    // Increase the '10' value to get a smoother/slower scroll!
-    window.scrollTo(0, c - c / 12);
-  }
-};
-
-// When the button is clicked, run our ScrolltoTop function above!
 scrollToTopButton.onclick = function (e) {
   e.preventDefault();
-  scrollToTop();
+  gsap.to(window, {
+    duration: 2,
+    ease: 'power3.inOut',
+    scrollTo: {
+        y: 0,
+        autoKill: true,
+    }
+  })
 };
 
 // style="shape-outside: url(img/White_Paper/Tanker_400.png);"
