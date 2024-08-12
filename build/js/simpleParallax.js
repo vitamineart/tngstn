5df8bcf52a32beb507e109b81e5abc82d340d194
Art <vitaminearte@gmail.com>
2024-08-12 16:48:12 +0300

    removed build folder

--- build/js/simpleParallax.js
+++
@@ -1,1 +0,0 @@
-const parallaxItems=document.querySelectorAll(".parallax");window.addEventListener("scroll",(()=>{let l=window.scrollY;parallaxItems.forEach((a=>{let e=a.dataset.speed;a.style.transform=`translateY(${l*e}px)`}))}));
